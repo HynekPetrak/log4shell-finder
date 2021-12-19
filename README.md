9e859f5a456264635e2e59449e5cb6389e394b73
HynekPetrak <hynek.petrak@gmail.com>
2021-12-19 01:51:38 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# log4shell_finder
+
+Python port of https://github.com/mergebase/log4j-detector
+
+Detects Log4J versions on your file-system within any application that are vulnerable to [CVE-2021-44228](https://mergebase.com/vulnerability/CVE-2021-44228/)  and [CVE-2021-45046](https://mergebase.com/vulnerability/CVE-2021-45046/). It is able to even find instances that are hidden several layers deep. Works on Linux, Windows, and Mac, and everywhere else Python runs, too!
+
+Currently reports `log4j-core` versions 2.12.2 and 2.17.0 as **SAFE**, 2.15.0 and 2.16.0 as **NOTOKAY** and all other versions as **VULNERABLE**
+(although it does report pre-2.0-beta9 as "**MAYBESAFE**").
+
+Can correctly detect log4j inside executable spring-boot jars/wars, dependencies blended
+into [uber jars](https://mergebase.com/blog/software-composition-analysis-sca-vs-java-uber-jars/), shaded jars, and even
+exploded jar files just sitting uncompressed on the file-system (aka *.class).
+
+## Usage
+
+Either run from a python interpreter or use the Windows/Linux binaries from the [dist](dist) folder.
+
+Does not need any extra python libraries.
+
+The binaries were produces with:
+
+```
+pip install pyinstaller
+pyinstaller -F ./test_log4shell.py
+```
+
+## Sample run
+
+```bash
+hynek@<redacted>:~/log4shell_finder$ ./test_log4shell.py ../war/log4j-samples/
+[2021-12-19 01:31:01,648] [INFO] [I] Starting ./test_log4shell.py ver. 1.1-20211219
+[2021-12-19 01:31:01,648] [INFO] [I] Parameters: ./test_log4shell.py ../war/log4j-samples/
+[2021-12-19 01:31:01,655] [INFO] [I] 'hostname': '<redacted>', 'fqdn': '<redacted>', 'ip': '<redacted>', 'system': 'Linux', 'release': '5.4.0-58-generic', 'version': '#64-Ubuntu SMP Wed Dec 9 08:16:25 UTC 2020', 'machine': 'x86_64', 'cpu': 'x86_64'
+[2021-12-19 01:31:01,655] [INFO] [I] Analyzing paths (could take a long time).
+[2021-12-19 01:31:01,658] [INFO] [*] [OLD] Package /home/hynek/war/log4j-samples/old-hits/log4j-1.1.3.jar contains Log4J-1.x <= 1.2.17
+[2021-12-19 01:31:01,662] [INFO] [*] [OLD] Package /home/hynek/war/log4j-samples/old-hits/log4j-1.2.17.jar contains Log4J-1.x <= 1.2.17
+[2021-12-19 01:31:01,666] [INFO] [*] [MAYBESAFE] Package /home/hynek/war/log4j-samples/old-hits/log4j-core-2.0-beta2.jar contains Log4J-2.x <= 2.0-beta8 (JndiLookup.class not present)
+[2021-12-19 01:31:01,679] [INFO] [*] [NOTOKAY] Package /home/hynek/war/log4j-samples/true-hits/log4j-core-2.15.0.jar contains Log4J-2.x == 2.15.0
+[2021-12-19 01:31:01,690] [INFO] [+] [VULNERABLE] Package /home/hynek/war/log4j-samples/true-hits/log4j-core-2.9.1.jar contains Log4J-2.x >= 2.0-beta9 (< 2.10.0)
+[2021-12-19 01:31:01,701] [INFO] [+] [VULNERABLE] Package /home/hynek/war/log4j-samples/true-hits/log4j-core-2.10.0.zip contains Log4J-2.x >= 2.10.0
+[2021-12-19 01:31:01,707] [INFO] [+] [VULNERABLE] Package /home/hynek/war/log4j-samples/true-hits/log4j-core-2.0-beta9.jar contains Log4J-2.x >= 2.0-beta9 (< 2.10.0)
+[2021-12-19 01:31:01,718] [INFO] [+] [VULNERABLE] Package /home/hynek/war/log4j-samples/true-hits/log4j-core-2.10.0.jar contains Log4J-2.x >= 2.10.0
+[2021-12-19 01:31:01,796] [INFO] [+] [VULNERABLE] Package /home/hynek/war/log4j-samples/true-hits/uber/infinispan-embedded-query-8.2.12.Final.jar contains Log4J-2.x >= 2.0-beta9 (< 2.10.0)
+[2021-12-19 01:31:01,900] [INFO] [+] [VULNERABLE] Folder /home/hynek/war/log4j-samples/true-hits/uber/expanded/org/apache/logging/log4j/core contains Log4J-2.x >= 2.0-beta9 (< 2.10.0)
+[2021-12-19 01:31:02,000] [INFO] [+] [VULNERABLE] Package /home/hynek/war/log4j-samples/true-hits/shaded/clt-1.0-SNAPSHOT.jar contains Log4J-2.x >= 2.10.0
+[2021-12-19 01:31:02,007] [INFO] [+] [VULNERABLE] Folder /home/hynek/war/log4j-samples/true-hits/shaded/expanded/clt/shaded/l/core contains Log4J-2.x >= 2.10.0
+[2021-12-19 01:31:02,036] [INFO] [+] [VULNERABLE] Folder /home/hynek/war/log4j-samples/true-hits/exploded/2.12.1/org/apache/logging/log4j/core contains Log4J-2.x >= 2.10.0
+[2021-12-19 01:31:02,887] [INFO] [+] [VULNERABLE] Package /home/hynek/war/log4j-samples/true-hits/springboot-executable/spiff-0.0.1-SNAPSHOT.zip:WEB-INF/lib/log4j-core-2.10.0.jar contains Log4J-2.x >= 2.10.0
+[2021-12-19 01:31:04,278] [INFO] [+] [VULNERABLE] Package /home/hynek/war/log4j-samples/true-hits/springboot-executable/spiff-0.0.1-SNAPSHOT.jar:WEB-INF/lib/log4j-core-2.10.0.jar contains Log4J-2.x >= 2.10.0
+[2021-12-19 01:31:05,712] [INFO] [+] [VULNERABLE] Package /home/hynek/war/log4j-samples/true-hits/springboot-executable/spiff-0.0.1-SNAPSHOT.ear:WEB-INF/lib/log4j-core-2.10.0.jar contains Log4J-2.x >= 2.10.0
+[2021-12-19 01:31:07,170] [INFO] [+] [VULNERABLE] Package /home/hynek/war/log4j-samples/true-hits/springboot-executable/spiff-0.0.1-SNAPSHOT.war:WEB-INF/lib/log4j-core-2.10.0.jar contains Log4J-2.x >= 2.10.0
+[2021-12-19 01:31:07,781] [INFO] [-] [NOTOKAY] Package /home/hynek/war/log4j-samples/false-hits/log4j-core-2.16.0.jar contains Log4J-2.x == 2.16.0
+[2021-12-19 01:31:07,805] [INFO] [-] [SAFE] Package /home/hynek/war/log4j-samples/false-hits/log4j-core-2.12.2.jar contains Log4J-2.x == 2.12.2
+[2021-12-19 01:31:07,984] [INFO] [-] [SAFE] Package /home/hynek/war/log4j-samples/false-hits/apache-log4j-2.17.0-bin.zip:apache-log4j-2.17.0-bin/log4j-core-2.17.0.jar contains Log4J-2.x >= 2.17.0
+[2021-12-19 01:31:08,442] [INFO] [-] [SAFE] Package /home/hynek/war/log4j-samples/false-hits/apache-log4j-2.17.0-bin/log4j-core-2.17.0.jar contains Log4J-2.x >= 2.17.0
+[2021-12-19 01:31:08,459] [INFO] [-] [SAFE] Folder /home/hynek/war/log4j-samples/false-hits/apache-log4j-2.17.0-bin/exploded/org/apache/logging/log4j/core contains Log4J-2.x >= 2.17.0
+[2021-12-19 01:31:08,493] [INFO] [-] [SAFE] Folder /home/hynek/war/log4j-samples/false-hits/exploded/2.12.2/org/apache/logging/log4j/core contains Log4J-2.x == 2.12.2
+[2021-12-19 01:31:08,523] [INFO] [I] Finished, found 12 vulnerable or unsafe log4j instances.
+```
---
+++ test_log4shell.py
@@ -0,0 +1,365 @@
+#!/usr/bin/env python3
+import argparse
+import os
+import mmap
+import sys
+import pathlib
+import logging
+import zipfile
+import socket
+import platform
+from enum import Enum
+
+VERSION = "1.1-20211219"
+
+log_name = 'log4shell_finder.log'
+
+# determine if application is a script file or frozen exe
+if getattr(sys, 'frozen', False):
+    application_path = os.path.dirname(sys.executable)
+elif __file__:
+    application_path = os.path.dirname(__file__)
+
+log_path = os.path.join(application_path, log_name)
+
+log = logging.getLogger("log4shell_finder")
+log.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+fh = logging.FileHandler(log_path)
+fh.setLevel(logging.INFO)
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+# create formatter and add it to the handlers
+formatter = logging.Formatter(
+    "[%(asctime)s] [%(levelname)s] %(message)s"
+)
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the log
+log.addHandler(fh)
+log.addHandler(ch)
+
+
+FILE_OLD_LOG4J = "log4j/dailyrollingfileappender.class"
+FILE_LOG4J_1 = "core/logevent.class"
+FILE_LOG4J_2 = "core/appender.class"
+FILE_LOG4J_3 = "core/filter.class"
+FILE_LOG4J_4 = "core/layout.class"
+FILE_LOG4J_5 = "core/loggercontext.class"
+FILE_LOG4J_2_10 = "appender/nosql/nosqlappender.class"
+FILE_LOG4J_VULNERABLE = "jndilookup.class"
+FILE_LOG4J_SAFE_CONDITION1 = "jndimanager.class"
+
+ACTUAL_FILE_LOG4J_2 = "core/Appender.class"
+ACTUAL_FILE_LOG4J_3 = "core/Filter.class"
+ACTUAL_FILE_LOG4J_4 = "core/Layout.class"
+ACTUAL_FILE_LOG4J_5 = "core/LoggerContext.class"
+ACTUAL_FILE_LOG4J_2_10 = "core/appender/nosql/NoSqlAppender.class"
+ACTUAL_FILE_LOG4J_JNDI_LOOKUP = "core/lookup/JndiLookup.class"
+ACTUAL_FILE_LOG4J_JNDI_MANAGER = "core/net/JndiManager.class"
+
+# This occurs in "JndiManager.class" in 2.15.0
+IS_LOG4J_SAFE_2_15_0 = b"Invalid JNDI URI - {}"
+
+# This occurs in "JndiManager.class" in 2.16.0
+IS_LOG4J_SAFE_2_16_0 = b"log4j2.enableJndi"
+
+# This occurs in "JndiLookup.class" in 2.17.0
+INSIDE_LOG4J_2_17_0 = b"JNDI must be enabled by setting log4j2.enableJndiLookup=true"
+
+# This occurs in "JndiLookup.class" before 2.12.2
+IS_LOG4J_NOT_SAFE_2_12_2 = b"Error looking up JNDI resource [{}]."
+
+verbose = False
+debug = False
+foundHits = False
+
+
+class FileType(Enum):
+    CLASS = 0
+    ZIP = 1
+    OTHER = -1
+
+
+ZIP_EXTS = [".zip", ".jar", ".war", ".ear", ".aar"]
+
+#
+# @param fileName
+# @return 0 == zip, 1 == class, -1 = who knows...
+#
+
+
+def get_file_type(file_name):
+    _, ext = os.path.splitext(file_name)
+    ext = ext.lower()
+    if ext == ".class":
+        return FileType.CLASS
+    if ext in ZIP_EXTS:
+        return FileType.ZIP
+    return FileType.OTHER
+
+
+def scan_archive(f, path=""):
+    log.debug(f"Scanning {path}")
+    with zipfile.ZipFile(f) as zf:
+        nl = zf.namelist()
+
+        isZip = False
+        log4jProbe = [False] * 5
+        isLog4j2_10 = False
+        hasJndiLookup = False
+        hasJndiManager = False
+        isLog4J1_X = False
+        isLog4j2_15 = False
+        isLog4j2_16 = False
+        isLog4j2_17 = False
+        isLog4j2_15_override = False
+        isLog4j2_12_2 = False
+        isLog4j2_12_2_override = False
+
+        for fn in nl:
+            fnl = fn.lower()
+
+            if fnl.endswith(tuple(ZIP_EXTS)):
+                with zf.open(fn, "r") as inner_zip:
+                    scan_archive(inner_zip, path=path+":"+fn)
+                    continue
+
+            if fnl.endswith(".class"):
+                if fnl.endswith(FILE_LOG4J_VULNERABLE):
+                    hasJndiLookup = True
+                    with zf.open(fn, "r") as inner_class:
+                        class_content = inner_class.read()
+                        if class_content.find(INSIDE_LOG4J_2_17_0) >= 0:
+                            isLog4j2_17 = True
+                        elif class_content.find(IS_LOG4J_NOT_SAFE_2_12_2) >= 0:
+                            isLog4j2_12_2_override = True
+                        else:
+                            isLog4j2_12_2 = True
+                elif fnl.endswith(FILE_LOG4J_SAFE_CONDITION1):
+                    hasJndiManager = True
+                    with zf.open(fn, "r") as inner_class:
+                        class_content = inner_class.read()
+                        if class_content.find(IS_LOG4J_SAFE_2_15_0) >= 0:
+                            isLog4j2_15 = True
+                            if class_content.find(IS_LOG4J_SAFE_2_16_0) >= 0:
+                                isLog4j2_16 = True
+                        else:
+                            isLog4j2_15_override = True
+                elif fnl.endswith(FILE_OLD_LOG4J):
+                    isLog4J1_X = True
+                elif fnl.endswith(FILE_LOG4J_1):
+                    log4jProbe[0] = True
+                elif fnl.endswith(FILE_LOG4J_2):
+                    log4jProbe[1] = True
+                elif fnl.endswith(FILE_LOG4J_3):
+                    log4jProbe[2] = True
+                elif fnl.endswith(FILE_LOG4J_4):
+                    log4jProbe[3] = True
+                elif fnl.endswith(FILE_LOG4J_5):
+                    log4jProbe[4] = True
+                elif fnl.endswith(FILE_LOG4J_2_10):
+                    isLog4j2_10 = True
+
+    log.debug(f" isZip = {isZip}, log4jProbe = {log4jProbe}, isLog4j2_10 = {isLog4j2_10}, hasJndiLookup = {hasJndiLookup}, hasJndiManager = {hasJndiManager}, isLog4J1_X = {isLog4J1_X}, isLog4j2_15 = {isLog4j2_15}, isLog4j2_16 = {isLog4j2_16}, isLog4j2_15_override = {isLog4j2_15_override}, isLog4j2_12_2 = {isLog4j2_12_2}, isLog4j2_12_2_override = {isLog4j2_12_2_override}, isLog4j2_17 = {isLog4j2_17} ")
+
+    isLog4j = False
+    isLog4j_2_10_0 = False
+    isLog4j_2_12_2 = False
+    isVulnerable = False
+    isSafe = False
+    if (log4jProbe[0] and log4jProbe[1] and log4jProbe[2] and
+            log4jProbe[3] and log4jProbe[4]):
+        isLog4j = True
+        if hasJndiLookup:
+            isVulnerable = True
+            if isLog4j2_10:
+                isLog4j_2_10_0 = True
+                if hasJndiManager:
+                    if (isLog4j2_17 or (isLog4j2_15 and not isLog4j2_15_override) or
+                            (isLog4j2_12_2 and not isLog4j2_12_2_override)):
+                        isSafe = True
+                        isLog4j_2_12_2 = (
+                            isLog4j2_12_2 and not isLog4j2_12_2_override)
+
+    if isLog4j:
+        log.debug(
+            f" isLog4j = {isLog4j}, isLog4j_2_10_0 = {isLog4j_2_10_0}, isLog4j_2_12_2 = {isLog4j_2_12_2}, isVulnerable = {isVulnerable}, isSafe = {isSafe},")
+        if isLog4J1_X:
+            prefix = f"[CRAZY] Package {path} contains Log4J-1.x AND Log4J-2.x"
+            foundLog4j1 = true
+        else:
+            prefix = f"Package {path} contains Log4J-2.x"
+        if isVulnerable:
+            if isLog4j_2_10_0:
+                if isSafe:
+                    if isLog4j2_17:
+                        buf = f"[-] [SAFE] {prefix} >= 2.17.0"
+                    elif isLog4j2_16:
+                        buf = f"[-] [NOTOKAY] {prefix} == 2.16.0"
+                    elif isLog4j_2_12_2:
+                        buf = f"[-] [SAFE] {prefix} == 2.12.2"
+                    else:
+                        buf = f"[*] [NOTOKAY] {prefix} == 2.15.0"
+                else:
+                    buf = f"[+] [VULNERABLE] {prefix} >= 2.10.0"
+            else:
+                buf = f"[+] [VULNERABLE] {prefix} >= 2.0-beta9 (< 2.10.0)"
+        else:
+            buf = f"[*] [MAYBESAFE] {prefix} <= 2.0-beta8 (JndiLookup.class not present) "
+        log.info(buf)
+        if not isSafe:
+            return 1
+        else:
+            return 0
+    elif isLog4J1_X:
+        log.info(
+            f"[*] [OLD] Package {path} contains Log4J-1.x <= 1.2.17")
+        return 1
+
+    return 0
+
+
+def check_class(f):
+    parent = pathlib.PurePath(f).parent
+    if f.lower().endswith(FILE_OLD_LOG4J):
+        log.info(
+            f"[*] [OLD] Folder {path} contains Log4J-1.x <= 1.2.17")
+        return 1
+
+    if not f.lower().endswith(FILE_LOG4J_1):
+        return 0
+
+    log.debug(f"[I] Match on {f}")
+    msg = f"Folder {parent} contains Log4J-2.x"
+
+    for fn in [ACTUAL_FILE_LOG4J_2, ACTUAL_FILE_LOG4J_3, ACTUAL_FILE_LOG4J_4,
+               ACTUAL_FILE_LOG4J_5, ACTUAL_FILE_LOG4J_JNDI_LOOKUP]:
+        if not os.path.exists(parent.parent.joinpath(fn)):
+            log.info(
+                f"[*] [MAYBESAFE] {msg} <= 2.0-beta8 (JndiLookup.class not present) ")
+            return 0
+
+    isVulnerable = True
+    if not os.path.exists(parent.parent.joinpath(ACTUAL_FILE_LOG4J_2_10)):
+        log.info(f"[+] [VULNERABLE] {msg} >= 2.0-beta9 (< 2.10.0)")
+        return 1
+    else:
+        # Check for 2.12.2...
+        fn = parent.parent.joinpath(ACTUAL_FILE_LOG4J_JNDI_LOOKUP)
+        if os.path.exists(fn):
+            with open(fn, "rb") as f:
+                with mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ) as mm:
+                    if mm.find(IS_LOG4J_NOT_SAFE_2_12_2) == -1:
+                        log.info(f"[-] [SAFE] {msg} == 2.12.2")
+                        return 0
+                    if mm.find(INSIDE_LOG4J_2_17_0) >= 0:
+                        log.info(f"[-] [SAFE] {msg} >= 2.17.0")
+                        return 0
+        fn = parent.parent.joinpath(ACTUAL_FILE_LOG4J_JNDI_MANAGER)
+        if os.path.exists(fn):
+            with open(fn, "rb") as f:
+                with mmap.mmap(f.fileno(), 0, prot=mmap.PROT_READ) as mm:
+                    if mm.find(IS_LOG4J_SAFE_2_16_0) >= 0:
+                        log.info(f"[-] [NOTOKAY] {msg} == 2.16.0")
+                        return 0
+                    elif mm.find(IS_LOG4J_SAFE_2_15_0) >= 0:
+                        log.info(f"[*] [NOTOKAY] {msg} == 2.15.0")
+                        return 1
+
+    log.info(f"[+] [VULNERABLE] {msg} >= 2.10.0")
+    return 1
+
+
+def process_file(filename):
+    hits = 0
+    try:
+        ft = get_file_type(filename)
+        if ft == FileType.OTHER:
+            return 0
+        elif ft == FileType.CLASS:
+            hits += check_class(filename)
+        elif ft == FileType.ZIP:
+            with open(filename, "rb") as f:
+                hits += scan_archive(f, filename)
+    except Exception as ex:
+        log.error(f"[E] Error processing {filename}: {ex}")
+    return hits
+
+
+def analize_directory(f):
+    hits = 0
+    f = os.path.realpath(f)
+    if os.path.isdir(f):
+        for (dirpath, dirnames, filenames) in os.walk(f):
+            for filename in filenames:
+                fullname = os.path.join(dirpath, filename)
+                hits += process_file(fullname)
+    elif os.path.isfile(f):
+        hits += process_file(f)
+    return hits
+
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = 'unknown'
+    finally:
+        s.close()
+    return IP
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Searches file system for vulnerable log4j version.',
+        usage='\tType "%(prog)s --help" for more information\n' +
+        '\tOn Windows "%(prog)s c:\\ d:\\"\n\tOn Linux "%(prog)s /"')
+    parser.add_argument('folders', nargs='+',
+                        help='List of folders or files to scan')
+    parser.add_argument('-d', '--debug', action="store_true",
+                        help='Increase verbosity, mainly for debugging purposes')
+
+    args = parser.parse_args()
+    if args.debug:
+        fh.setLevel(logging.DEBUG)
+        ch.setLevel(logging.DEBUG)
+
+    log.info(f"[I] Starting {__file__} ver. {VERSION}")
+    log.info("[I] Parameters: " + " ".join(sys.argv))
+    system, node, release, version, machine, processor = platform.uname()
+    hi = {'hostname': socket.gethostname(),
+          'fqdn': socket.getfqdn(),
+          'ip': get_ip(),
+          'system': system,
+          'release': release,
+          'version': version,
+          'machine': machine,
+          'cpu': processor,
+          }
+    log.info(f"[I] {str(hi).strip('{}')}")
+
+    for fn in args.folders:
+        if not os.path.exists(fn):
+            log.error(f"[E] Invalid path: [{fn}]")
+            sys.exit(102)
+
+    log.info("[I] Analyzing paths (could take a long time).")
+
+    hits = 0
+    for f in args.folders:
+        hits += analize_directory(f)
+
+    log.info(f"[I] Finished, found {hits} vulnerable or unsafe log4j instances.")
+    if hits:
+        sys.exit(2)
+    else:
+        log.info(
+            f"[I] No vulnerable Log4J 2.x samples found in supplied paths: {args.folders}")
+
+
+main()
